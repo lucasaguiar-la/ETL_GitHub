4e750a9678bfd4ed63df254bc9473de0d9df3efb
lucasaguiar-la <lucas.aguiar.ads@gmail.com>
2025-03-17 20:23:27 -0300

    feat: KaggleHub download method for GitHub dataset

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from src.transformation.transformer import DataTransformer
 
 import logging
 import os
+import kagglehub
+import shutil
 
 def main():
     timestamp = datetime.now().strftime('%d%m%Y_%H%M%S')
@@ -14,10 +16,27 @@ def main():
     setup_logging(log_file)
     logger = logging.getLogger('app_loger')
 
+    os.makedirs("data", exist_ok=True)
+    path = kagglehub.dataset_download("nikhil25803/github-dataset")
+    dest_path = os.path.join("data/raw/", os.path.basename(path))
+    shutil.move(path, dest_path)
+
+    folder_path = os.path.join(dest_path)
+
+    if os.path.exists(folder_path) and os.path.isdir(folder_path):
+        for file_name in os.listdir(folder_path):
+            src_file = os.path.join(folder_path, file_name)
+            dest_file = os.path.join("data/raw", file_name)
+            shutil.move(src_file, dest_file)
+
+        os.rmdir(folder_path)
+        logging.info(f'Dataset salvo em: {dest_file}')
+
     logger.info('Iniciando pipeline ETL para dados do GitHub...')
 
+    '''
     try:
-        logger.info('Iniciando etapa de extração')
+        logger.info('\nIniciando etapa de extração')
         extractor = DataExtractor('data/raw/repository_data.csv') # DATASET ORIGINAL (mais pesasado)
         # extractor = DataExtractor('data/raw/github_dataset.csv') # DATASET DE TESTES (mais leve)
         raw_data = extractor.extract()
@@ -30,6 +49,7 @@ def main():
     except Exception as e:
         logger.error(f'Erro durante a execução do pipeline: {str(e)}')
         raise
+    '''
 
 if __name__ == '__main__':
     main()
