af269565ef7035ff830090d7d696d795d97bbcea
lucasaguiar-la <lucas.aguiar.ads@gmail.com>
2025-03-19 20:36:56 -0300

    fix: Adjusting datetime and column values in transformation class

--- main.py
+++ main.py
@@ -17,16 +17,27 @@ def main():
     file_path = 'data\\raw\\repository_data.csv'
     os.makedirs("data", exist_ok=True)
 
+    # Etapa de EXTRAÇÃO
     logger.info('Iniciando etapa de extração dos dados...')
     extractor = DataExtractor(file_path)
 
-    # Etapa de EXTRAÇÃO
     try:
         if extractor.extract():
             raw_data = extractor.check_extraction()
             logger.info(f'Extração concluída. {len(raw_data)} registros encontrados.')
     except Exception as e:
         logger.error(f'Erro durante a extração de dados: {str(e)}')
+
+    # Etapa de TRANSFORMAÇÃO
+    logger.info('Iniciando etapa de tratamento dos dados...')
+    transformer = DataTransformer(raw_data)
+
+    try:
+        if transformer.transform():
+            logging.info(f'Tratamento concluído com sucesso!')
+    except Exception as e:
+        logger.error(f'Erro durante o tratamento de dados: {str(e)}')
+
 
 if __name__ == '__main__':
     main()
--- src/transformation/transformer.py
+++ src/transformation/transformer.py
@@ -11,69 +11,73 @@ class DataTransformer:
         self.logger = logging.getLogger('app_logger')
 
     def transform(self):
-        logging.info('Iniciando transformação dos dados...')
+        self.logger.info('Iniciando transformação dos dados...')
 
         df = self.data.copy()
 
+        if not pd.api.types.is_datetime64_any_dtype(df['created_at']):
+            df['created_at'] = pd.to_datetime(df['created_at'])
+        if df['created_at'].dt.tz is None:
+            df['created_at'] = df['created_at'].dt.tz_localize('UTC', ambiguous='infer')
+
         date_now = pd.Timestamp.now(tz='UTC')
-        df['created_at'] = df['created_at'].datetime.tz_localize('UTC', ambiguous='infer')
-        df['time_age'] = (date_now - df['created_at']).datetime.days
+        df['time_age'] = (date_now - df['created_at']).dt.days
 
         df = self.clean_data(df)
         df = self.apply_filter(df)
-        df = self.calculate_engagement(df)
+        # df = self.calculate_engagement(df)
 
     def clean_data(self, df):
-        logging.info('Realizando a limpeza de dados.')
+        self.logger.info('Realizando a limpeza de dados.')
 
         try:
             duplicates = df.duplicated().sum()
             if duplicates > 0:
-                logging.info(f'Removendo {duplicates} linhas duplicadas.')
+                self.logger.info(f'Removendo {duplicates} linhas duplicadas.')
                 df = df.drop_duplicates()
 
             null_counts = df.isnull().sum()
-            if null_counts > 0:
-                logging.info(f'Tratando valores nulos em {sum(null_counts > 0)} colunas.')
+            if null_counts.any():
+                self.logger.info(f'Tratando valores nulos em {sum(null_counts > 0)} colunas.')
 
                 numeric_cols = df.select_dtypes(include=['int64', 'float64']).columns
                 for col in numeric_cols:
                     if df[col].isnull().sum() > 0:
                         median_value = df[col].median()
-                        df[col] = df[col].fillna(median_value)
-                        logging.info(f"Colina '{col}': valores nulos substituídos pela mediana ({median_value})")
+                        df.loc[:, col] = df[col].fillna(median_value)
+                        self.logger.info(f"Colina '{col}': valores nulos substituídos pela mediana ({median_value})")
 
-                text_col = df.select_types(include=['object']).columns
+                text_col = df.select_dtypes(include=['object']).columns
                 for col in text_col:
                     if df[col].isnull().sum() > 0:
-                        df[col] = df[col].fillna('Desconhecido')
-                        logging.info(f"Coluna '{col}': valores nulos substituídos por 'Desconhecido'")
+                        df.loc[:, col] = df[col].fillna('Desconhecido')
+                        self.logger.info(f"Coluna '{col}': valores nulos substituídos por 'Desconhecido'")
 
             return df
         except Exception as e:
-            logging.error(f'Erro na etapa de limpeza de dados: {str(e)}')
+            self.logger.error(f'Erro na etapa de limpeza de dados: {str(e)}')
             raise
 
     def apply_filter(self, df):
-        logging.info(f'Aplicando filtro de linguagem: {FILTER_KEY}')
+        self.logger.info(f'Aplicando filtro de linguagem: {FILTER_KEY}')
         return df[df['primary_language'] == FILTER_KEY]
 
     def calculate_engagement(self, df):
-        logging.info('Calculando métricas de engajamento...')
+        self.logger.info('Calculando métricas de engajamento...')
 
         try:
-            logging.info('Normalizando métricas com log para lidar com outliers.')
+            self.logger.info('Normalizando métricas com log para lidar com outliers.')
 
             for col in ['stars_count', 'forks_count', 'watchers', 'pull_requests', 'commit_count']:
                 df[f'{col}_norm'] = np.log1p(df)
-                logging.info(f"Coluna '{col}': normalizada com log1p")
+                self.logger.info(f"Coluna '{col}': normalizada com log1p")
 
-            logging.info('Calculando taxas por idade do projeto.')
+            self.logger.info('Calculando taxas por idade do projeto.')
             df['commit_rate'] = df['commit_count'] / (df['time_age'] + 1)
             df['pr_rate'] = df['pull_requests'] / (df['time_age'] + 1)
             df['fork_rate'] = df['forks_count'] / (df['time_age'] + 1)
 
-            logging.info('Calculando score final de engajamento com pesos.')
+            self.logger.info('Calculando score final de engajamento com pesos.')
             df['engagement_score'] = (
                 df['stars_count_norm'] * 0.35 +
                 df['forks_count_norm'] * 0.20 +
@@ -84,9 +88,9 @@ class DataTransformer:
                 df['pr_rate'] * 100 * 0.05
             )
 
-            logging.info('Cálculo de engajamento concluído com sucesso!')
+            self.logger.info('Cálculo de engajamento concluído com sucesso!')
             return df
         
         except Exception as e:
-            logging.error(f'Erro no cálculo de métricas de engajametno: {str(e)}')
+            self.logger.error(f'Erro no cálculo de métricas de engajametno: {str(e)}')
             raise
